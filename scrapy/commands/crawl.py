92d443c7965263f0fa6cd54d9d8446e0c244e059
Rolando Espinoza La fuente <darkrho@gmail.com>
2012-11-20 23:31:21 -0400

    commands: added -c option and urls support to crawl command.
    It allows to execute the spider against an specific url with an
    arbitrary callback.
    
    Example: scrapy crawl myspider -c parse_item http://site/id/123

--- scrapy/commands/crawl.py
+++ scrapy/commands/crawl.py
@@ -7,7 +7,7 @@ class Command(ScrapyCommand):
     requires_project = True
 
     def syntax(self):
-        return "[options] <spider>"
+        return "[options] <spider> [start url] ..."
 
     def short_desc(self):
         return "Start crawling from a spider or URL"
@@ -16,6 +16,7 @@ class Command(ScrapyCommand):
         ScrapyCommand.add_options(self, parser)
         parser.add_option("-a", dest="spargs", action="append", default=[], metavar="NAME=VALUE", \
             help="set spider argument (may be repeated)")
+        parser.add_option("-c", dest="callback", help="use this callback in the start urls")
         parser.add_option("-o", "--output", metavar="FILE", \
             help="dump scraped items into FILE (use - for stdout)")
         parser.add_option("-t", "--output-format", metavar="FORMAT", default="jsonlines", \
@@ -37,9 +38,29 @@ class Command(ScrapyCommand):
     def run(self, args, opts):
         if len(args) < 1:
             raise UsageError()
-        elif len(args) > 1:
-            raise UsageError("running 'scrapy crawl' with more than one spider is no longer supported")
         spname = args[0]
+        urls = args[1:]
         spider = self.crawler.spiders.create(spname, **opts.spargs)
-        self.crawler.crawl(spider)
+        requests = self._requests(spider, urls, opts.callback) if urls else None
+        self.crawler.crawl(spider, requests)
         self.crawler.start()
+
+    def _resolve_callback(self, spider, cbname):
+        try:
+            callback = getattr(spider, cbname)
+            if not callable(callback):
+                raise UsageError("Callback '{0}' is not a method.",
+                                 print_help=False)
+            return callback
+        except AttributeError:
+            raise UsageError("Callback '{0}' not found in the"
+                             " spider '{1}'".format(cbname, spider.name),
+                             print_help=False)
+
+    def _requests(self, spider, urls, callback):
+        if callback:
+            callback = self._resolve_callback(spider, callback)
+        for url in urls:
+            req = spider.make_requests_from_url(url)
+            req.callback = callback
+            yield req
